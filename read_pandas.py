3cddd73083ebca5489d386d2f52659f4be855767
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-02-07 21:44:10 +0100

    functional app
    Print button, generate domain column and download button is added

--- read_pandas.py
+++ read_pandas.py
@@ -1,22 +1,39 @@
 import pandas as pd
 
-from pathlib import Path
+# from pathlib import Path
 
-p = Path(__file__)
-data_path = str(p.cwd())+'\data\\'
+# p = Path(__file__)
+# data_path = str(p.cwd())+'\data\\'
 
-file_input = data_path+'data_input.csv'
+# file_input = data_path+'data_input.csv'
 
-df = pd.read_csv(file_input)
+import streamlit as st
 
-print(type(df))
-print(type(df['email']))
-df['domain'] = df['email'].apply(lambda x : x.split('@')[1])
-print(df['domain'].value_counts(normalize=True).plot())
-print(df['domain'].info())
+file_input = st.file_uploader('Please upload your file', type=['csv','txt'])    
 
-# d = {'a': 1, 'b': 2, 'c': 3}
-# ser = pd.Series(data=d, index=['a', 'b', 'c'])
+# df= None
+
+if file_input:
+    df = pd.read_csv(file_input)
+    df['domain'] = df['email'].apply(lambda x : x.split('@')[1])
+    
+if st.button('Print columns'):
+    st.write(df['domain'])
+    
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+if st.button('Generate file '):
+    csv = convert_df(df['domain'])
+    # name = 
+    st.download_button(
+        label="Download data as CSV",   
+        data=csv,
+        file_name='my_df.csv'),
+        mime='text/csv')
+
 
 
 # print(ser.apply(lambda x: x*2))
